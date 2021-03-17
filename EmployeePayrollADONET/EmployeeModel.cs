5b121b721a2d2cb8c31172c1d36d8b8acb5b373d
diliprathod32 <diliprathod32@gmail.com>
2021-03-17 23:56:52 +0530

    [Dilip] RetrievesDataFromDatabase

---
+++ EmployeePayrollADONET/EmployeeModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollADONET
+{
+    public class EmployeeModel
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; } 
+        public string Gender { get; set; }
+        public DateTime StartDate { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public double PhoneNumber { get; set; } 
+        public int BasicPay { get; set; }
+        public double Deduction { get; set; }
+        public double TaxablePay { get; set; }
+        public double IncomeTax { get; set; }
+        public int NetPay { get; set; }
+       
+        public override bool Equals(object obj)
+        {
+            EmployeeModel employee = (EmployeeModel)obj;// as EmployeeModel;
+            if (employee == null)
+                return false;
+            return employee.EmpID == EmpID && employee.EmpName == EmpName && employee.Gender == Gender && employee.Department == Department
+                && employee.Salary == Salary && employee.StartDate == StartDate;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
--- EmployeePayrollADONET/Program.cs
+++ EmployeePayrollADONET/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace EmployeePayrollADONET
 {
     class Program
     {
+        static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=payroll_service;User Id=root;Password=root";
+
+        public static List<EmployeeModel> GetAllEmployeePayrollData()
+        {
+            EmployeeModel employee;
+            List<EmployeeModel> EmployeeModelList = new List<EmployeeModel>();
+
+            SqlConnection connection = new SqlConnection(connectionString); 
+            SqlCommand command = new SqlCommand("dbo.GetEmployeePayrollAllData", connection);
+            //command.CommandType = CommandType.Text;
+            command.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                connection.Open();
+                using (connection)
+                {
+                   
+                    SqlDataReader rd = command.ExecuteReader();
+                   
+                    while (rd.Read())
+                    {
+                        employee = new EmployeeModel();
+                        employee.EmpID = rd.IsDBNull(0) ? default : rd.GetInt32(0);
+                        employee.EmpName = rd.IsDBNull(1) ? default : rd.GetString(1);
+                        employee.Gender = rd.IsDBNull(2) ? default : rd.GetString(2);
+                        employee.StartDate = rd.IsDBNull(3) ? default : rd.GetDateTime(3);
+                        employee.Salary = rd.IsDBNull(4) ? default : rd.GetDouble(4);
+                        employee.Department = rd.IsDBNull(5) ? default : rd.GetString(5);
+                        EmployeeModelList.Add(employee);
+                    }
+                    return EmployeeModelList;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return null;
+        }
+        public static void PrintEmployeeData(EmployeeModel employee)
+        {
+            Console.WriteLine("EmpID".PadRight(12) + ": " + employee.EmpID);
+            Console.WriteLine("EmpName".PadRight(12) + ": " + employee.EmpName);
+            Console.WriteLine("Gender".PadRight(12) + ": " + employee.Gender);
+            Console.WriteLine("Start date".PadRight(12) + ": " + employee.StartDate);
+            Console.WriteLine("Salary".PadRight(12) + ":" + employee.Salary);
+            Console.WriteLine("Department".PadRight(12) + ": " + employee.Department);
+        }
+        public static void PrintEmployeeData(List<EmployeeModel> employeeList)
+        {
+            if (employeeList != null)
+            {
+                foreach (var employee in employeeList)
+                {
+                    PrintEmployeeData(employee);
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to employee payroll ado .net");
+            Console.WriteLine("welcome to Employee Payroll Service");
+
+            PrintEmployeeData(GetAllEmployeePayrollData());
+            Console.ReadLine();
         }
     }
 }
